48399dd10ac01f15ca7713fa8869a32337c9527a
gabekole <gabriel.koleszar@uconn.edu>
2022-11-23 22:50:04 -0500

    Pixijs webpack example

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 3000
+    },
+}
