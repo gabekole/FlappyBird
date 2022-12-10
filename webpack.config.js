dcd1e05c69b1f175b815df4ad0d505c67a36ef00
Gabe Koleszar <72774655+gabekole@users.noreply.github.com>
2022-12-10 01:29:44 +0000

    :bug: Add html plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -34,4 +35,9 @@ module.exports = {
               },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'public/index.html'
+        })
+    ],
 }
