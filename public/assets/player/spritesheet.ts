80836f8acf157c113e804c36b24fe144240258b8
gabekole <gabriel.koleszar@uconn.edu>
2022-12-13 12:20:54 -0500

    Squashed commit of the following:
    commit 82d21d8e456bdfceb0db7581e17873a773a8bd3f
    Author: gabekole <gabriel.koleszar@uconn.edu>
    Date:   Tue Dec 13 12:14:06 2022 -0500
    
        :hammer: Add npm command to serve build
    
    commit 8084177a5f451172cef8ead66a8da569a8925dcb
    Author: gabekole <gabriel.koleszar@uconn.edu>
    Date:   Tue Dec 13 12:05:00 2022 -0500
    
        :fire: Preliminary animation completed
    
    commit 570eaf0d2cc2586c952ebde88410ff04c66c133c
    Author: gabekole <gabriel.koleszar@uconn.edu>
    Date:   Tue Dec 13 00:38:33 2022 -0500
    
        :new: add animated texture images

---
+++ public/assets/player/spritesheet.ts
@@ -0,0 +1,82 @@
+import img from '../player/spritesheet.png'
+
+const sheet = {"frames": {
+"frame1.png":
+{
+	"frame": {"x":0,"y":0,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame2.png":
+{
+	"frame": {"x":533,"y":0,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame3.png":
+{
+	"frame": {"x":1066,"y":0,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame4.png":
+{
+	"frame": {"x":0,"y":422,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame5.png":
+{
+	"frame": {"x":533,"y":422,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame6.png":
+{
+	"frame": {"x":1066,"y":422,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame7.png":
+{
+	"frame": {"x":0,"y":844,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+},
+"frame8.png":
+{
+	"frame": {"x":533,"y":844,"w":533,"h":422},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":533,"h":422},
+	"sourceSize": {"w":533,"h":422}
+}},
+"animations": {
+	"frame": ["frame1.png","frame2.png","frame3.png","frame4.png","frame5.png","frame6.png","frame7.png","frame8.png"]
+},
+"meta": {
+	"app": "https://www.codeandweb.com/texturepacker",
+	"version": "1.0",
+	"image": img,
+	"format": "RGBA8888",
+	"size": {"w":1599,"h":1266},
+	"scale": "1",
+	"smartupdate": "$TexturePacker:SmartUpdate:293ecee2ee1ab68fdaedaea729e65ea4:dc335368addc429b2a87c330988e27cd:729adc6043343cfda41c447ce8f464d6$"
+}
+}
+
+export default sheet
